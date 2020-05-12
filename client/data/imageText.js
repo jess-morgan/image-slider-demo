9ef1f51b3d49397bfe030feab84638d61f602fd0
Jessica Morgan <jmorgan@aucklandmuseum.com>
2020-05-12 12:46:34 +1200

    move data folder

---
+++ client/data/imageText.js
@@ -0,0 +1,126 @@
+const imageText = {
+    images: [
+        {
+        id: 0,
+        title1: "Albert St ",
+        date1: " circa 1865",
+        credit1: "Reproduction from abumen silver print",
+        title2: "Albert St 2019",
+        credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+      },
+        {
+        id: 1,
+        title1: "Corner of Queen and Fort streets ",
+        date1: " 1865",
+        credit1: "Reproduction from abumen silver print",
+        title2: "Corner of Queen and Fort streets 2019",
+        credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+       },
+         {
+            id: 2,
+            title1: "Grafton Rd looking towards the harbour ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Grafton Rd looking towards the harbour 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+         {
+            id: 3,
+            title1: "Grey St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Greys Ave and Aotea Sq 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 4,
+            title1: "High St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "High St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 5,
+            title1: "Karangahape Rd ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Karangahape Rd 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 6,
+            title1: "Pitt St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Pitt St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 7,
+            title1: "Queen St from corner of Customs St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Queen St from corner of Customs St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 8,
+            title1: "Queen St between Vulcan Lane and Durham St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Queen St between Vulcan Lane and Durham St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 9,
+            title1: "Shortland St looking down to Queen St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Shortland St looking down to Queen St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 10,
+            title1: "Swanson St from Queen St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Swanson St from Queen St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 11,
+            title1: "Symonds St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Symonds St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 12,
+            title1: "Corner of Victoria and Hobson streets ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Corner of Victoria and Hobson streets 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 13,
+            title1: "Victoria St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Victoria St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        },
+        {
+            id: 14,
+            title1: "Wyndham St from Queen St ",
+            date1: " circa 1865",
+            credit1: "Reproduction from abumen silver print",
+            title2: "Wyndham St from Queen St 2019",
+            credit2: "By Andrew Pettengell. Inkjet on Iford Galeria Metallic."
+        }
+  ]
+}
+
+export default imageText
